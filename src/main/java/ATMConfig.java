af14789be70565eafeacbf392e15807da145b8da
6110400858 <atthakorn.na@ku.th>
2020-09-14 16:54:22 +0700

    initial commit

---
+++ src/main/java/ATMConfig.java
@@ -0,0 +1,25 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource(){
+        return  new DataSourceFile();
+    }
+
+    @Bean
+    public Bank bank(){
+        return  new Bank("MY Bank",dataSource());
+    }
+
+    @Bean
+    public  ATM atm(){
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI(){
+        return new AtmUI(atm());
+    }
+}
